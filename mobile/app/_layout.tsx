e8b7464649e181485ab199d8859ee69d17b21ddd
Pravenn18 <insanepraveen2018@gmail.com>
2025-05-28 04:11:45 +0530

    added NoInternet card in the root layout such that if internet goes then it should the relevant modal.

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,6 +1,16 @@
-// app/_layout.tsx
+import NoInternetCard from '@/components/NoInternetModa';
+import { ThemeProvider } from '@/context/themeContext';
 import { Stack } from 'expo-router';
+import { View } from 'react-native';
 
+//TODO: NOT TESTED NO INTERNET CARD, NEED TO GIVE PREVIW BUILD FOR THAT
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <ThemeProvider>
+      <View style={{ flex: 1 }}>
+        <NoInternetCard />
+        <Stack screenOptions={{ headerShown: false }} />
+      </View>
+    </ThemeProvider>
+  );
 }
