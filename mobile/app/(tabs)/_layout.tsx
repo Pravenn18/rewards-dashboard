63d488ae9112f365e1c7a45ba049ba4235b90006
Pravenn18 <insanepraveen2018@gmail.com>
2025-05-28 04:12:21 +0530

    modified home layout such that it will fetch data when internet came back

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -1,15 +1,34 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, Platform, Text } from 'react-native';
 
 import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { rewardsService } from '@/services/rewardService';
+import { useNetworkStore } from '@/store/networkState';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+  const [loading, isLoading] = useState(false);
+  
+  const { fetchPoints, fetchTransactions, fetchCategories} = rewardsService;
+  const isConnected = useNetworkStore((state) => state.isConnected);
+
+  useEffect(() => {
+    const fetch = async() => {
+      isLoading(true);
+      await fetchPoints();
+      await fetchTransactions();
+      await fetchCategories();
+      isLoading(false);
+    }
+    fetch();
+  }, [!isConnected])
+
+  if(loading) return <ActivityIndicator size="large" style={{flex: 1}}/>
 
   return (
     <Tabs
@@ -20,7 +39,6 @@ export default function TabLayout() {
         tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
           ios: {
-            // Use a transparent background on iOS to show the blur effect
             position: 'absolute',
           },
           default: {},
