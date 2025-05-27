e731764d3ba0719590ccc480f5dcad11c3980422
Pravenn18 <insanepraveen2018@gmail.com>
2025-05-27 18:29:40 +0530

    removed unused code from points and transaction apis such that they will not throw errors on vercel deployement.

--- backend/app/api/rewards/points/route.ts
+++ backend/app/api/rewards/points/route.ts
@@ -1,7 +1,7 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import { transactions } from '@/data/transactions';
 
-export async function GET(req: NextRequest) {
+export async function GET() {
   const totalPoints = transactions.reduce((sum, t) => sum + t.points, 0);
   return NextResponse.json({ points: totalPoints });
 }
--- backend/app/api/rewards/transaction/route.ts
+++ backend/app/api/rewards/transaction/route.ts
@@ -1,7 +1,7 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import { transactions } from '@/data/transactions';
 
-export async function GET(req: NextRequest) {
+export async function GET() {
   const latestData = transactions.slice(0, 10);
   return NextResponse.json({ transactions: latestData });
 }
