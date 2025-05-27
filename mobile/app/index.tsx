7974a53de587a617a91061aab37857fc27f1babf
Pravenn18 <insanepraveen2018@gmail.com>
2025-05-28 04:13:02 +0530

    added sunscription in entry firle so the it will keep live track of internet state

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -2,12 +2,24 @@ import { Redirect } from 'expo-router';
 import { useEffect, useState } from 'react';
 import { getStoredUser } from '../services/authService';
 import { useAuthStore } from '../store/authStore';
+import { ActivityIndicator } from 'react-native';
+import NetInfo  from "@react-native-community/netinfo";
+import { useNetworkStore } from '@/store/networkState';
 
 export default function Index() {
   const [checking, setChecking] = useState(true);
   const user = useAuthStore((s) => s.user);
   const setUser = useAuthStore((s) => s.setUser);
+  const setIsConnected = useNetworkStore((state) => state.setIsConnected);
 
+  useEffect(() => {
+  const unsubscribe = NetInfo.addEventListener(state => {
+    setIsConnected(!!state.isConnected)
+  });
+  return(() => {
+    unsubscribe();
+  })
+  })
   useEffect(() => {
     const checkAuth = async () => {
       const data = await getStoredUser();
@@ -17,7 +29,9 @@ export default function Index() {
     checkAuth();
   }, []);
 
-  if (checking) return null;
+  if (checking) return <ActivityIndicator size="small" style={{flex: 1}}/>
 
-  return <Redirect href={user ? '/(tabs)' : '/(auth)/login'} />;
+  return (
+    <Redirect href={user ? '/(tabs)' : '/(auth)/login'} />
+  )
 }
